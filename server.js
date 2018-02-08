ff2ef668e721746cf2b27a5fbe8db4a413f3c38d
StreakInTheSky <streakinthesky@gmail.com>
2018-02-08 06:36:06 -0800

    connect db, add error handling, and add body parser

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const mongoose = require('mongoose')
-
-mongoose.Promise = global.Promise
+const bodyParser = require('body-parser')
 
 const app = express()
 
+app.use(bodyParser.json())
 app.use(express.static('public'))
+
+// error handling
+app.use((err, req, res, next)=>{
+  console.error(err.message)
+  res.status(err.status || 500).send(err.message)
+})
+
+mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost/basic-login-test', (err)=>{
+  if (err) {
+    console.error(err)
+  } else {
+    console.log('connected to db')
+  }
+})
 
 app.listen(process.env.PORT || 8080, ()=> console.log(
   `Your app is listening on port ${process.env.PORT || 8080}`))
+  .on('error', err=>{
+    mongoose.disconnect();
+    console.error(err)
+  })
