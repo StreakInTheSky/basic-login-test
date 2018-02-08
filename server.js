f4fb4ce45e71f28b0260a0b2df5e42e353d34da7
StreakInTheSky <streakinthesky@gmail.com>
2018-02-08 05:48:07 -0800

    edit for clarity

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+
 const app = express()
 
 app.use(express.static('public'))
+
 app.listen(process.env.PORT || 8080, ()=> console.log(
   `Your app is listening on port ${process.env.PORT || 8080}`))
