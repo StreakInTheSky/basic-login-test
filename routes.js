ca6a0538bb9a409d34c56fd453b1bdb226f12397
StreakInTheSky <streakinthesky@gmail.com>
2018-02-08 07:06:42 -0800

    Create POST endpoint

--- routes.js
+++ routes.js
@@ -1,3 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const User = require('model')
+
+router.post('/', (req, res, next)=>{
+  console.log(req.body)
+}
+
+module.exports = router
