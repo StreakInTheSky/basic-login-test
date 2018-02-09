497c54b2a9204bc2d739dba3340c41e96b3db1de
StreakInTheSky <streakinthesky@gmail.com>
2018-02-08 23:31:54 -0800

    Setup POST with error handling

--- routes.js
+++ routes.js
@@ -3,7 +3,32 @@ const router = express.Router()
 const User = require('./model')
 
 router.post('/', (req, res, next)=>{
-  console.log(req.body)
+  const requiredFields = ['email', 'password']
+  const missingField = requiredFields.find(field => !(field in req.body))
+
+  if (missingField) {
+    return next({
+      status: 422,
+      message: `${missingField} is required`
+    })
+  }
+
+  return User
+    .find({email: req.body.email})
+    .then(result=>{
+      if(result.length > 0){
+        return Promise.reject({
+          status: 422,
+          message: 'A user with that email already exists'
+        })
+      }
+
+      console.log({
+        email: req.body.email,
+        password: req.body.password
+      })
+    })
+    .catch(err=>next(err))
 })
 
 module.exports = router
