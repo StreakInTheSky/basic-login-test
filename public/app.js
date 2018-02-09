a910bfcfc4ab0b88d84e0b510df4c8a5c6b0e1b5
StreakInTheSky <streakinthesky@gmail.com>
2018-02-09 09:17:00 -0800

    Create forms and ajax calls

---
+++ public/app.js
@@ -0,0 +1,69 @@
+var state = {
+  signupEmail: '',
+  signupPassword: '',
+  loginEmail: '',
+  loginPassword: ''
+}
+
+function detectFieldChange() {
+	$('form :input').change(function(){
+		let input = $(this).attr('name')
+		state[input] = $(this).val()
+	})
+}
+
+function signup() {
+  var data = {
+    email: state.signupEmail,
+    password: state.signupPassword
+  }
+  var settings = {
+      url: 'api',
+      data: JSON.stringify(data),
+      contentType: 'application/json; charset=utf-8'
+  }
+
+  $.post(settings)
+    .done(function() {
+      console.log('success')
+      $('.dump').text('User Created')
+    })
+    .fail(function(xhr, status, errorThrown) {
+      console.log('failure')
+      $('.dump').text(xhr.responseText)
+    })
+}
+
+function login() {
+  var settings = {
+    url: 'api',
+    headers: {
+      "Authorization": "Basic " + btoa(state.loginEmail + ":" + state.loginPassword)
+    }
+  }
+
+  $.get(settings)
+    .done(function(data){
+      $('.dump').text('Signed in as ' + data.email)
+    })
+    .fail(function(xhr, status, errorThrown) {
+      $('.dump').text(xhr.responseText)
+    })
+}
+
+function buttonListeners() {
+  $('#signup-button').click(function(event){
+    event.preventDefault()
+    signup()
+  })
+  $('#login-button').click(function(event){
+    event.preventDefault()
+    login()
+  })
+
+}
+
+$(function() {
+  buttonListeners()
+  detectFieldChange()
+})
